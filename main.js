06930d113ffde17a1fcedfc51a9f345f057f1a8c
Anabella <55283296+Anabella-91@users.noreply.github.com>
2023-03-08 23:58:17 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+const menu = document.querySelector(".menu");
+const links = document.querySelector(".nav_links");
+const icon_close = document.querySelector(".close");
+const text_index = document.querySelector(".text_index");
+const footer_text = document.querySelector(".footer_text");
+
+menu.addEventListener("click", openMenu);
+
+function openMenu() {
+	links.classList.toggle("animationFromTop");
+	icon_close.classList.toggle("open");
+	menu.classList.toggle("close");
+	text_index.classList.toggle("close");
+	footer_text.classList.toggle("close");
+}
+
+icon_close.addEventListener("click", (e) => {
+	if (e.target.id === "nav") {
+		links.classList.remove("animationFromTop");
+		links.classList.toggle("removeAnimation");
+		icon_close.classList.remove("open");
+	}
+	menu.classList.remove("close");
+});
